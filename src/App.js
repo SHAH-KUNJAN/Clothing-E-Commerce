5f859e97f94e4013bf54a610d5ea3fe3c23f2b07
Kunjan Shah <kunjannshah310@gmail.com>
2023-05-18 17:39:32 +0530

    Updated shop page and added collection preview and item page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS Page</h1>
-    </div>
-  )
-}
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/hats' Component={HatsPage} />
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/shop' Component={ShopPage} />
       </Routes>
     </div>
   );
