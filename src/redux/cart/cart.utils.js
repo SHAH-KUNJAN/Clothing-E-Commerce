9e7a07425416c4b1c62eafbd68706054b5eb86b4
Kunjan Shah <kunjannshah310@gmail.com>
2023-07-17 17:24:28 +0530

    Added Cart Remove Item Functionality.

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,19 @@ export const addItemToCart = (currentCartItem, newCartItem) => {
 
   return [...currentCartItem, { ...newCartItem, quantity: 1 }];
 };
+
+export const removeItemFromCart = (currentCartItem, removeCartItem) => {
+  const existCartItem = currentCartItem.find(
+    (item) => item.id === removeCartItem.id
+  );
+
+  if (existCartItem.quantity === 1) {
+    return currentCartItem.filter((item) => item.id !== removeCartItem.id);
+  }
+
+  return currentCartItem.map((item) =>
+    item.id === removeCartItem.id
+      ? { ...item, quantity: item.quantity - 1 }
+      : item
+  );
+};
